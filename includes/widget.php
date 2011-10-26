c0f64670f149e7f13256e6f2c25e522b252dcc87
DigitalLeap <DigitalLeap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-26 11:53:47 +0000

    tagging version 0.4.2
    git-svn-id: https://plugins.svn.wordpress.org/the-social-links/tags/0.4.2@455896 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/widget.php
+++ includes/widget.php
@@ -40,11 +40,11 @@ class TheSocialLinkWidget extends WP_Widget{
 				<?php foreach($social_networks as $slug => $name):?>
 					<?php $tsl_link = get_option('tsl_'.$slug);?>
 					<?php if ( !empty( $tsl_link ) ):?>
-						<a href="<?php echo $tsl_link;?>" rel="nofollow" target="<?php echo get_option('tsl_link_target');?>"><img src="<?php echo $plugin_path;?>/icons/<?php echo $icon_size.'/'.$slug;?>.png" title="<?php echo $name;?>" alt="<?php echo $name;?>" /></a>
+						<a href="<?php echo $tsl_link;?>" rel="nofollow" target="<?php echo get_option('tsl_link_target');?>"><img src="<?php echo $plugin_path;?>icons/<?php echo $icon_size.'/'.$slug;?>.png" title="<?php echo $name;?>" alt="<?php echo $name;?>" /></a>
 					<?php endif;?>
 				<?php endforeach;?>
 				<?php if(get_option('tsl_display_credit')):?>
-					<br /><a style="font-size: 0.8em;" href="http://www.seagyndavis.com/wordpress/plugin/the-social-links/">Provided by The Social Links</a>
+					<br /><a style="font-size: 0.8em;" href="http://digitalleap.co.za/wordpress/plugin/the-social-links/">Provided by The Social Links</a>
 				<?php endif;?>
 			<?php echo $after_widget; ?>
         <?php
